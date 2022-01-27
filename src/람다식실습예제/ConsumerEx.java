a495842c60eab053460c3abbdc7e8d778adedfc3
JongIn Yoon <bellpp93@gmail.com>
2022-01-27 16:50:35 +0900

    0127_Hex

--- src/람다식실습예제/ConsumerEx.java
+++ src/람다식실습예제/ConsumerEx.java
@@ -3,6 +3,7 @@ package 람다식실습예제;
 import java.util.function.Consumer;
 
 public class ConsumerEx {
+	
 	public static void main(String[] args) {
 		Consumer<String> consumer = t -> System.out.print(t + "8");  //Java8 출력
 		consumer.accept("Java");
