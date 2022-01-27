c47c569a80ea73bca930e16878e87ad4d514c722
JongIn Yoon <bellpp93@gmail.com>
2022-01-27 16:51:34 +0900

    0127_수정

--- src/람다식실습예제/ConsumerEx.java
+++ src/람다식실습예제/ConsumerEx.java
@@ -5,7 +5,7 @@ import java.util.function.Consumer;
 public class ConsumerEx {
 	
 	public static void main(String[] args) {
-		Consumer<String> consumer = t -> System.out.print(t + "8");  //Java8 출력
+		Consumer<String> consumer = t -> System.out.print(t + "8");  // Java8 출력
 		consumer.accept("Java");
 	}
 }
