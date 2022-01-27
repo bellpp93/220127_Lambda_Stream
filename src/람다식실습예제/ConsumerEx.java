e58a2f9a635f6db5d4fd46aad954732b29966a69
JongIn Yoon <bellpp93@gmail.com>
2022-01-27 16:54:11 +0900

    0127_한글오류수정

--- src/람다식실습예제/ConsumerEx.java
+++ src/람다식실습예제/ConsumerEx.java
@@ -7,5 +7,6 @@ public class ConsumerEx {
 	public static void main(String[] args) {
 		Consumer<String> consumer = t -> System.out.print(t + "8");  // Java8 출력
 		consumer.accept("Java");
+		// Consumer 소비자 => 매개값은 있고, 리턴값은 없다.
 	}
 }
